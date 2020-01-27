91a662cc67942deeb5e7f4a87ee2d4ecee212175
KubaBBB <boliglow@student.agh.edu.pl>
2020-01-27 21:51:31 +0100

    added generator of graphs

--- router-game.py
+++ router-game.py
@@ -9,6 +9,29 @@ PACKETS_STD_MEAN = THROUGHPUT_META_STD
 ALPHA = 2
 BETA = 0.1
 
+
+def relation_generator(num_of_agents, num_of_coalitions, length):
+    result = []
+
+    index = 0
+    for agent_id in range(num_of_agents):
+        coalition = np.random.randint(0, num_of_coalitions)
+        l = [agent_id, coalition]
+        result.append(l)
+        index += 1
+
+    if index < length:
+        while(index < length):
+
+            agent_id = np.random.randint(0, num_of_agents)
+            coalition = np.random.randint(0, num_of_coalitions)
+            l = [agent_id, coalition]
+            result.append(l)
+            index += 1
+
+    return result
+
+
 class Coalition():
     def __init__(self, id):
         self.members = []
@@ -33,7 +56,10 @@ class Coalition():
             self.current_throughput_pool += member.throughput
 
     def remove_member(self, member):
-        self.members.remove(member)
+        try:
+            self.members.remove(member)
+        except:
+            pass
         self.recalculate()
 
     def join_coalition(self, member):
@@ -175,10 +201,13 @@ class Game:
             coal_num = np.random.randint(0, len(agent.neighbour_coalitions))
             agent.neighbour_coalitions[coal_num].members.append(agent)
             agent.current_coalition = coal_num
-            if coal_num in self.coalitions:
-                self.coalitions[coal_num].agents_count[agent.id] += 1
-            else:
-                self.coalitions[coal_num].agents_count[agent.id] = 1
+            try:
+                if coal_num in self.coalitions:
+                    self.coalitions[coal_num].agents_count[agent.id] += 1
+                else:
+                    self.coalitions[coal_num].agents_count[agent.id] = 1
+            except Exception as e:
+                print(str(e))
 
             agent.current_coalition_ptr = agent.neighbour_coalitions[coal_num]
             index +=1
@@ -265,11 +294,13 @@ class Game:
 if __name__ == "__main__":
     agents = [15]
     coalitions = [3]
-    steps = [30]
-    relations = [[[0,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,1],[7,1],[8,1],[9,1],[10,1],[11,1],[12,1],[13,1],[14,1],[0,2],[1,2],[6,2],[7,2],[8,2],[9,2],[3,2],[4,2]]]  
+    steps = [20]
+    relations = [[0,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,1],[7,1],[8,1],[9,1],[10,1],[11,1],[12,1],[13,1],[14,1],[0,2],[1,2],[6,2],[7,2],[8,2],[9,2],[3,2],[4,2]]
+    r = relation_generator(agents[0], coalitions[0], 50)
     for step in steps:
         for coalition in coalitions:
             for agent in agents:
-                g = Game(agent, coalition, relations[0])
+                g = Game(agent, coalition, r)
                 g.play(step)
+
 
